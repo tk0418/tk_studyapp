65dea9233845fec5a461f0e8a4eac58abfb1cc7f
tk0418 <fwsoccer9418@gmail.com>
2024-01-21 18:13:58 +0900

    リレーション実装

--- app/Models/original_Post.php
+++ app/Models/original_Post.php
@@ -25,3 +25,30 @@ class Post extends Model
     ];
 }
 
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    
+    protected $fillable = [
+        'title',
+        'unit',
+        'purpose',
+        'point',
+        'body',
+        'image_url',
+        'reference',
+        'memo',
+        'test',
+        'solution',
+    ];
+}
